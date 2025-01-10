6b400e390417e429301e48ea6bc1fbad5ee095ff
Brennen Heinrich <contact@bheinri.ch>
2025-01-10 17:10:01 -0600

    P:

--- scripts/scan.sh
+++ scripts/scan.sh
@@ -1,10 +1,1 @@
-printf "\n====== SCANNING FOR CREDIT CARD NUMBERS ======\n"
-grep -rE --color=always '(\b[0-9]{4}[- ]?){3}[0-9]{4}\b' . --exclude-dir={.git} --line-number
-echo "========= CREDIT CARD SCAN COMPLETE =========="
-printf "\n==== SCANNING FOR SOCIAL SECURITY NUMBERS ====\n"
-grep -rE --color=always '\b[0-9]{3}-[0-9]{2}-[0-9]{4}\b' . --exclude-dir={.git} --line-number
-echo "======= SOCIAL SECURITY SCAN COMPLETE ========"
-printf "\n========= SCANNING FOR PHONE NUMBERS =========\n"
-grep -rE --color=always '\b[0-9]{3}-[0-9]{3}-[0-9]{4}\b' . --exclude-dir={.git} --line-number
-grep -rE --color=always '\([0-9]{3}\) [0-9]{3}-[0-9]{4}' . --exclude-dir={.git} --line-number
-echo "========= PHONE NUMBER SCAN COMPLETE ========="
+# TODO: write the script
